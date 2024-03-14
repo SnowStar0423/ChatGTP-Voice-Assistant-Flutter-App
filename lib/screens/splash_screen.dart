673c01e9f9a1afc945dca374c71ca003c4d75508
SnowStar0423 <snow55view@gmail.com>
2024-03-14 02:57:55 +0000

    initial commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -9,7 +9,7 @@ import 'home_page.dart';
 class SplashScreen extends StatefulWidget {
   static const String routeName = '/splash_screen';
 
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -32,44 +32,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xFFCCCCFF),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-              color: const Color(0xFFCCCCFF),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.shade500,
-                    offset: const Offset(4, 4),
-                    blurRadius: 15,
-                    spreadRadius: 1
-                ),
-                const BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-4, -4),
-                    blurRadius: 15,
-                    spreadRadius: 1
-                ),
-              ]
-          ),
-          alignment: Alignment.center,
-          height: 40,
-          //color: const Color(0xFFCCCCFF),
-          child: const Text("Powered By NextDigit",
-            style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
-        ),
         body: Container(
           alignment: Alignment.center,
-          //color: const Color(0xFFCCCCFF),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // const Text(
-              //   'Welcome!',
-              //   style: TextStyle(
-              //       fontSize: 34,
-              //       fontWeight: FontWeight.w700,
-              //       color: Colors.black),
-              // ),
-              // const SizedBox(height: 30,),
               Container(
                 padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
